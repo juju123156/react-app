abd5c21c476a47a6bbaa13d14bc1a7ca6263623a
juju123156 <fnqllove11@naver.com>
2023-04-19 13:27:29 +0900

    add modal

--- reactStudy/react/my-app/src/pages/Mypage.js
+++ reactStudy/react/my-app/src/pages/Mypage.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import Component from "../component/Component";
 import SelectBox from "../UI/SelectBox";
 import Button from "../UI/Button";
+import Modal from "../UI/Modal";
 
 const OPTION_PAGENUM = [
   { value: 10, name: "10" },
@@ -13,23 +14,23 @@ const OPTION_PAGENUM = [
   { value: 1000, name: "1000" },
 ];
 
-const OPTION_EQP_CL_CD= [
+const OPTION_EQP_CL_CD = [
   { value: "", name: "전체" },
   { value: "01", name: "DU" },
   { value: "02", name: "RU" },
   { value: "03", name: "DUH" },
   { value: "04", name: "DUL" },
-  { value: "05", name: "NODEB" }
+  { value: "05", name: "NODEB" },
 ];
 
-const OPTION_EQP_OP_STAT= [
+const OPTION_EQP_OP_STAT = [
   { value: "", name: "전체" },
   { value: 3, name: "대기" },
   { value: 4, name: "운용" },
   { value: 9, name: "미운용" },
 ];
 
-const OPTION_JRDT_HDOFC_CD= [
+const OPTION_JRDT_HDOFC_CD = [
   { value: "", name: "전체" },
   { value: "04", name: "연구소" },
   { value: "11", name: "영업그룹" },
@@ -41,7 +42,7 @@ const OPTION_JRDT_HDOFC_CD= [
   { value: "39", name: "Corporate Center" },
 ];
 
-const OPTION_RDT_TEAM_ORG_CD= [
+const OPTION_RDT_TEAM_ORG_CD = [
   { value: "", name: "전체" },
   { value: "05", name: "플랫폼연구팀" },
   { value: "06", name: "UI솔루션연구팀" },
@@ -74,122 +75,95 @@ const OPTION_RDT_TEAM_ORG_CD= [
   { value: "42", name: "UX팀" },
 ];
 
-
 const Mypage = () => {
-
   // 버튼 클릭 on off
   const [isClicked, setIsClicked] = useState(false);
+  // 모달창 노출 여부
+  const [modalOpen, setModalOpen] = useState(false);
 
-const clickHandler = (isClicked, e) => {
-  console.log(isClicked);
-  setIsClicked(isClicked => !isClicked);  // on off
-  addData();
-}
-
-// axios post add data test
-const addData = () => {
-  let addData =
-    {
-  
-          "EQP_ID" : "test"
-        , "EQP_NM" : "test"
-        , "EQP_CL_CD" : "test"
-        , "EQP_OP_STAT_CD" : "test"
-        , "JRDT_HDOFC_CD" : "test"
-        , "RDT_TEAM_ORG_CD" : "test"
-        , "EQP_SRNO" : "test"
-        , "MST_IP" : "test"
-        , "LAT_CODN" : 123
-        , "LNG_CODN" : 456
-        , "OP_CHRR_ID" : "test"
-        , "REGRT_DT" : "2023-04-18 11:02:23"
-        , "REGRT_ID" : "testId"
-        , "UDT_ID" : "testId"
-        , "UDT_DT" : "2023-04-18 11:02:23"
-      
+  const clickHandler = (isClicked, e) => {
+    console.log(isClicked);
+    setIsClicked((isClicked) => !isClicked); // on off
+    addData();
   };
 
-       
-  axios({
-    params: {
-   
-  
-          // EQP_ID : "test_id",
-          EQP_NM : "test"
-        , EQP_CL_CD : "t"
-        , EQP_OP_STAT_CD : "t"
-        , JRDT_HDOFC_CD : "t"
-        , RDT_TEAM_ORG_CD : "t"
-        , EQP_SRNO : "test"
-        , MST_IP : "test"
-        , LAT_CODN : 123
-        , LNG_CODN : 456
-        , OP_CHRR_ID : "test"
-        , REGRT_DT : "2023-04-18 11:02:23"
-        , REGRT_ID : "testId"
-        , UDT_ID : "testId"
-        , UDT_DT : "2023-04-18 11:02:23"
-      
-    }
-    ,method: "post",
-    url: "/api/insEqpInf",
-    // header에서 JSON 타입의 데이터라는 것을 명시
-    headers: {'Content-type': 'application/json'}
-              // 추가
-          , "Access-Control-Allow-Origin": `http://localhost:8080`
-          , "Access-Control-Allow-Credentials": "true"
-        
-}).then((res)=>{
-    alert("성공");
-    // API로 부터 받은 데이터 출력
-    console.log(res.data);
-}).catch(error=>{
-    console.log("실패");
-    console.log(addData);
-});
+  const showModal = () => {
+    setModalOpen(true);
+  };
 
+  // axios post add data test
+  const addData = () => {
+    let addData = {
+      EQP_ID: "test",
+      EQP_NM: "test",
+      EQP_CL_CD: "test",
+      EQP_OP_STAT_CD: "test",
+      JRDT_HDOFC_CD: "test",
+      RDT_TEAM_ORG_CD: "test",
+      EQP_SRNO: "test",
+      MST_IP: "test",
+      LAT_CODN: 123,
+      LNG_CODN: 456,
+      OP_CHRR_ID: "test",
+      REGRT_DT: "2023-04-18 11:02:23",
+      REGRT_ID: "testId",
+      UDT_ID: "testId",
+      UDT_DT: "2023-04-18 11:02:23",
+    };
 
-  // axios
-  //   .post(
-  //     "/api/insEqpInf"
-  //     , {
-  //         method: "post"
-  //       , data: addData
-  //       , baseURL: "http://localhost:3000"
-  //       , headers: {
-  //         "Content-Type": `application/json;charset=UTF-8`
-  //         , Accept: "application/json"
-
-  //         // 추가
-  //         , "Access-Control-Allow-Origin": `http://localhost:8080`
-  //         , "Access-Control-Allow-Credentials": "true"
-  //       }
-  //     ,
-  //   })
-  //   .then((response) => {
-  //     // add된 데이터 표에 표시하는 건  나중에 처리
-  //     // setRowData(response.data); 
-  //     alert("성공");
-  //     console.log(response.data);
-  //   }).catch(error => {
-  //     console.log(addData);
-  //     alert("실패");
-  //   })
-  //   ;
-}
-
-
+    axios({
+      params: {
+        // EQP_ID : "test_id",
+        EQP_NM: "test",
+        EQP_CL_CD: "t",
+        EQP_OP_STAT_CD: "t",
+        JRDT_HDOFC_CD: "t",
+        RDT_TEAM_ORG_CD: "t",
+        EQP_SRNO: "test",
+        MST_IP: "test",
+        LAT_CODN: 123,
+        LNG_CODN: 456,
+        OP_CHRR_ID: "test",
+        REGRT_DT: "2023-04-18 11:02:23",
+        REGRT_ID: "testId",
+        UDT_ID: "testId",
+        UDT_DT: "2023-04-18 11:02:23",
+      },
+      method: "post",
+      url: "/api/insEqpInf",
+      // header에서 JSON 타입의 데이터라는 것을 명시
+      headers: { "Content-type": "application/json" },
+      // 추가
+      "Access-Control-Allow-Origin": `http://localhost:8080`,
+      "Access-Control-Allow-Credentials": "true",
+    })
+      .then((res) => {
+        alert("성공");
+        // API로 부터 받은 데이터 출력
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log("실패");
+        console.log(addData);
+      });
+  };
 
   return (
     <Fragment>
+      {modalOpen && <Modal setModalOpen={setModalOpen} />}
       <h1>Mypage</h1>
       <SelectBox options={OPTION_PAGENUM}></SelectBox>
       <SelectBox options={OPTION_EQP_CL_CD} defaultValue=""></SelectBox>
       <SelectBox options={OPTION_EQP_OP_STAT} defaultValue=""></SelectBox>
       <SelectBox options={OPTION_JRDT_HDOFC_CD} defaultValue=""></SelectBox>
       <SelectBox options={OPTION_RDT_TEAM_ORG_CD} defaultValue=""></SelectBox>
-      <Button name="조회" isClicked={isClicked} clickHandler={clickHandler}></Button>
+      <Button
+        name="조회"
+        isClicked={isClicked}
+        clickHandler={clickHandler}
+      ></Button>
       <Component />
+      <button onClick={showModal}>모달띄우기</button>
     </Fragment>
   );
 };
